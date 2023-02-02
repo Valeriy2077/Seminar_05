e880c890f3a5a93e32fe6d1d4750a8a30cde9462
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-02-03 02:06:02 +0300

    task34

--- Task34/Task34.cs
+++ Task34/Task34.cs
@@ -2,3 +2,34 @@
 
 // [345, 897, 568, 234] -> 2
 
+Console.WriteLine("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[size];
+FillArrayRandomNumbers(num);
+PrintArray(num);
+int count = 0;
+
+for (int i = 0; i < num.Length; i++)
+if (num[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"Чётных чисел в массиве - {count} ");
+
+void FillArrayRandomNumbers(int[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] num)
+
+{
+    Console.Write("[ ");
+    for(int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
